3905a33695ab966933e474e82ac6fc975fab15ca
DBPerera22 <denukaperera49@gmail.com>
2022-10-29 01:13:35 +0530

    server modules change

--- product/controllers/controller.js
+++ product/controllers/controller.js
@@ -1,15 +1,15 @@
 const product = require('../models/product')
 
 
-const getProducts = async (req,res)=>{
+ getProducts = async (req,res)=>{
 
-    // product.find()
-    // .then(data =>{
-    //     return res.status(200).send({data:data})
-    // })
-    // .catch(err =>{
-    //     return res.status(401).send({err:err})
-    // })
+    product.find()
+    .then(data =>{
+        return res.status(200).send({data:data})
+    })
+    .catch(err =>{
+        return res.status(401).send({err:err})
+    })
 
     try{
         const data = await product.find()
@@ -19,8 +19,64 @@ const getProducts = async (req,res)=>{
         return res.status(500).send({error:error})
     }
     
-}
+},
+
+addProduct = async function (req, res) {
+        if (
+          !req.body.category ||
+          !req.body.brand ||
+          !req.body.gender ||
+          !req.body.price ||
+          !req.body.qty ||
+          !req.body.color || 
+          !req.body.seller ||
+          !req.body.sizes || 
+          !req.body.shippingType ||
+          !req.body.seller
+
+        ) {
+          res.json({ success: false, msg: "Enter all fields" });
+        } else {
+          var newProduct = product({
+            category: req.body.category,
+            brand: req.body.brand,
+            gender: req.body.gender,
+            price: req.body.price,
+            qty: req.body.qty,
+            color: req.body.color,
+            sizes: req.body.sizes,
+            shippingType: req.body.shippingType,
+            seller: req.body.seller,
+            
+          });
+          newProduct.save(function (err, newProduct) {
+            if (err) {
+              res.json({ success: false, msg: "Failed to save" });
+            } else {
+              res.json({ success: true, msg: "Successfully saved" });
+            }
+          });
+        }
+      },
+
+
+
+    deleteProduct = async function (req, res) {
+            try {           
+                const id = req.params.id;
+    
+                  const products = await product.findByIdAndDelete(id);
+                  if (!products) {
+                    throw createError(404, 'Product does not exist');
+                  }
+                  res.send(products);
+                } catch (err) {
+                  res.send("Error" + err);
+                }
+              },
+
+
 
 module.exports = {
-    getProducts
+    functions
 }
